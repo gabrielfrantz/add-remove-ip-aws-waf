6870beb42e196f4941ef8668a8c2907eede5d694
gabrielfrantz <gabrieldecarvalhofrantz@gmail.com>
2025-05-13 17:15:28 -0300

    wip

--- update_ipset.py
+++ update_ipset.py
@@ -1,24 +1,45 @@
 import boto3
+import ipaddress
 import os
 import sys
 
-ip_address = sys.argv[1]
-action = sys.argv[2]
-region_name = os.environ['AWS_REGION']
+try:
+    ip_cidr = sys.argv[1]
+    ip_network = ipaddress.ip_network(ip_cidr, strict=False)
+except ValueError:
+    print(f"ERRO: O valor '{sys.argv[1]}' não é um IP/CIDR válido. Ex: 192.168.0.1/32")
+    sys.exit(1)
 
-waf = boto3.client('wafv2', region_name)
+action = sys.argv[2].lower()
+
+region_name = os.environ['AWS_REGION']
 ip_set_id = os.environ['AWS_WAF_IPSET_ID']
 ip_set_name = os.environ['AWS_WAF_IPSET_NAME']
 scope = 'REGIONAL'
+
+waf = boto3.client('wafv2', region_name=region_name)
 
 response = waf.get_ip_set(Name=ip_set_name, Scope=scope, Id=ip_set_id)
 addresses = response['IPSet']['Addresses']
 lock_token = response['LockToken']
 
-if action == "add" and f"{ip_address}/32" not in addresses:
-    addresses.append(f"{ip_address}/32")
-elif action == "rem" and f"{ip_address}/32" in addresses:
-    addresses.remove(f"{ip_address}/32")
+if action == "add":
+    if ip_cidr in addresses:
+        print(f"ERRO: O IP {ip_cidr} já está cadastrado no IPSet.")
+        sys.exit(1)
+    addresses.append(ip_cidr)
+    print(f"P {ip_cidr} adicionado com sucesso.")
+
+elif action == "rem":
+    if ip_cidr not in addresses:
+        print(f"ERRO: O IP {ip_cidr} não está presente no IPSet.")
+        sys.exit(1)
+    addresses.remove(ip_cidr)
+    print(f"IP {ip_cidr} removido com sucesso.")
+
+else:
+    print("ERRO: Ação inválida. Use 'add' ou 'rem'.")
+    sys.exit(1)
 
 waf.update_ip_set(
     Name=ip_set_name,
