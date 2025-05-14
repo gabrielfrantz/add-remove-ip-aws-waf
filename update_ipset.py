ee833ed863c4eb86fadfb92d59554d47718584d6
Gabriel de Carvalho Frantz <33354703+gabrielfrantz@users.noreply.github.com>
2025-05-14 15:25:09 -0300

    Update update_ipset.py

--- update_ipset.py
+++ update_ipset.py
@@ -3,14 +3,30 @@ import ipaddress
 import os
 import sys
 
-try:
-    ip_cidr = sys.argv[1]
-    ip_network = ipaddress.ip_network(ip_cidr, strict=False)
-except ValueError:
-    print(f"ERRO: O valor '{sys.argv[1]}' não é um IP/CIDR válido. Ex: 192.168.0.1/32")
+ip_cidr = sys.argv[1]
+action = sys.argv[2].lower()
+
+if '/' not in ip_cidr:
+    print(f"❌ ERRO: O IP '{ip_cidr}' está sem o barramento CIDR. Ex: 192.168.0.1/32")
     sys.exit(1)
 
-action = sys.argv[2].lower()
+try:
+    ip_network = ipaddress.ip_network(ip_cidr, strict=False)
+except ValueError:
+    print(f"❌ ERRO: O valor '{ip_cidr}' não é um IP/CIDR válido. Ex: 192.168.0.1/32")
+    sys.exit(1)
+
+valid_prefixes_v4 = [8, 16, 24, 32]
+valid_prefixes_v6 = [24, 32, 48, 56, 64, 128]
+
+if isinstance(ip_network, ipaddress.IPv4Network):
+    if ip_network.prefixlen not in valid_prefixes_v4:
+        print(f"❌ ERRO: Prefixo CIDR '{ip_network.prefixlen}' inválido para IPv4. Use um dos seguintes: /8, /16, /24, /32")
+        sys.exit(1)
+#elif isinstance(ip_network, ipaddress.IPv6Network):
+#    if ip_network.prefixlen not in valid_prefixes_v6:
+#        print(f"❌ ERRO: Prefixo CIDR '{ip_network.prefixlen}' inválido para IPv6. Use um dos seguintes: /24, /32, /48, /56, /64, /128")
+#        sys.exit(1)
 
 region_name = os.environ['AWS_REGION']
 ip_set_id = os.environ['AWS_WAF_IPSET_ID']
