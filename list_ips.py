89ea229fdff4a73dab19b1678c8e7fa4439cfa5d
Gabriel de Carvalho Frantz <33354703+gabrielfrantz@users.noreply.github.com>
2025-05-23 09:28:34 -0300

    Update list_ips.py

--- list_ips.py
+++ list_ips.py
@@ -11,6 +11,7 @@ waf = boto3.client('wafv2', region_name=region_name)
 response = waf.get_ip_set(Name=ip_set_name, Scope=scope, Id=ip_set_id)
 addresses = response['IPSet']['Addresses']
 
-print("IPs cadastrados no IPSet:")
+print(f"Total de IPs cadastrados no IPSet: {len(addresses)}\n")
+print("Lista dos IPs cadastrados no IPSet:")
 for ip in addresses:
     print(f"- {ip}")
