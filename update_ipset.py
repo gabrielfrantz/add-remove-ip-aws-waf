2ef3b4ac2f5d13b801f15e91a35149674c79af5b
Gabriel de Carvalho Frantz <33354703+gabrielfrantz@users.noreply.github.com>
2025-06-04 10:19:57 -0300

    wip

--- update_ipset.py
+++ update_ipset.py
@@ -44,16 +44,13 @@ malicious_list_name = os.environ.get('AWS_WAF_MALICIOUS_LIST_NAME')
 
 waf = boto3.client('wafv2', region_name=region_name)
 
-if action == "adicionar" and malicious_list_id and malicious_list_name:
-    response = waf.get_ip_set(Name=malicious_list_name, Scope=scope, Id=malicious_list_id)
-    malicious_list_addresses = response['IPSet']['Addresses']
-    malicious_networks = [ipaddress.ip_network(ip) for ip in malicious_list_addresses]
+malicious_list_addresses = set(response['IPSet']['Addresses'])
+malicious_blocked_set = set(malicious_list_addresses)
 
-    for ip in valid_ips:
-        ip_net = ipaddress.ip_network(ip)
-        if ip_net in malicious_networks:
-            print(f"❌ ERRO: O IP {ip} está presente no IPSet ({malicious_list_name}) e não pode ser adicionado.")
-            sys.exit(1)
+for ip in valid_ips:
+    if ip in malicious_blocked_set:
+        print(f"❌ ERRO: O IP {ip} está presente no IPSet ({malicious_list_name}) e não pode ser adicionado.")
+        sys.exit(1)
 
 response = waf.get_ip_set(Name=country_exceptions_list_name, Scope=scope, Id=country_exceptions_list_id)
 addresses = response['IPSet']['Addresses']
